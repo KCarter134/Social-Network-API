428021df36dd60e0ed44a4062eb0960f6ffb3a6c
Kyle Carter <kcarter134@yahoo.com>
2022-10-18 16:19:50 -0400

    updated models index

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Thoughts = require('./thoughts');
+const User = require('./user');
+
+// Export Modules: Users and Thoughts
+module.exports = { User, Thoughts };
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Require express and mongoose
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -6,16 +7,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
+app.use(require('./routes'));
+
+// Connect mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-// Use this to log mongo queries being executed!
+// Log mongoose queries
 mongoose.set('debug', true);
 
-app.use(require('./routes'));
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
